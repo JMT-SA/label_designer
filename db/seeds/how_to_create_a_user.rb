b99d5c2b75169d09fcdc729d5afb8dc0e922be0d
James Silberbauer <jamessil@telkomsa.net>
2018-01-23 21:49:17 +0200

    Use updated roda data grid gem.

--- db/seeds/how_to_create_a_user.rb
+++ db/seeds/how_to_create_a_user.rb
@@ -1,11 +1,11 @@
 # Follow these steps to create a user via bin/console
 #
 # require 'bcrypt'
-# usr = { login_name: 'usr_login',
-#         user_name: 'User Name',
-#         email: 'user@example.com',
-#         password_hash: BCrypt::Password.create('user_password')
+# usr = { login_name: 'login_name',
+#         user_name: 'USer Name',
+#         email: nil,
+#         password_hash: BCrypt::Password.create('new_password')
 #       }
 # repo = UserRepo.new
-# repo.create(usr)
+# repo.create(:users, usr)
 raise 'This is not for running!'
