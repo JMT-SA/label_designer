24cc368ceea9dc6f222bfcd177527549c9046f9c
James Silberbauer <jamessil@telkomsa.net>
2018-01-25 17:55:12 +0200

    Use current folder for puma binding.

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 environment 'production'
-bind 'unix:///home/nsld/label_designer/tmp/sockets/label-designer-puma.sock'
+bind 'unix:///home/nsld/label_designer/current/tmp/sockets/label-designer-puma.sock'
 # pidfile '/home/nsld/label_designer/tmp/puma/pid'
 # state_path 'home/nsld/label_designer/tmp/puma/state'
 activate_control_app
