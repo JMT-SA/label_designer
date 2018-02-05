aa964e92042a1d5dfc6785d16e6fb9507fea2c45
James Silberbauer <jamessil@telkomsa.net>
2018-02-05 13:07:13 +0200

    Make puma config work more universally.

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,8 @@
 environment 'production'
-bind 'unix:///home/nsld/label_designer/current/tmp/sockets/label-designer-puma.sock'
+root_dir = File.expand_path('../../', __FILE__)
+bind "unix://#{root_dir}/tmp/sockets/label-designer-puma.sock"
+directory root_dir
 # pidfile '/home/nsld/label_designer/tmp/puma/pid'
 # state_path 'home/nsld/label_designer/tmp/puma/state'
-activate_control_app
+stdout_redirect "#{root_dir}/log/stdout", "#{root_dir}/log/stderr", true
+# activate_control_app
