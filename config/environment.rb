4f3fbb0f667a423811420ef266e642417f9adf90
James Silberbauer <jamessil@telkomsa.net>
2018-06-24 15:31:06 +0200

    Add Sequel's DB extension to validate connections.

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,7 @@ require 'logger'
 DB = Sequel.connect(db_name)
 DB.logger = Logger.new($stdout) if ENV.fetch('RACK_ENV') == 'development' && !ENV['DONOTLOGSQL']
 # DB.logger = Logger.new('log/sql.log') if ENV.fetch('RACK_ENV') == 'development' && !ENV['DONOTLOGSQL']
+DB.extension(:connection_validator) # Ensure connections are not lost over time.
 DB.extension :pg_array
 DB.extension :pg_json
 DB.extension :pg_hstore
