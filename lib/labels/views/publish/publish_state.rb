1955c90bd2a1acc7064e49997ec11c1a53f84e41
James Silberbauer <jamessil@telkomsa.net>
2019-03-22 12:52:07 +0200

    On publish, do not show SQL if notifications are in place

--- lib/labels/views/publish/publish_state.rb
+++ lib/labels/views/publish/publish_state.rb
@@ -4,30 +4,51 @@ module Labels
   module Publish
     module Batch
       class PublishState
-        def self.call(res) # rubocop:disable Metrics/AbcSize
+        def self.call(res)
           label_states = res.body
-          header_cols = label_states.map { |h| [h[:destination], "#{h[:destination]}<br>#{h[:server_ip]}"] }.uniq
-          cols = header_cols.map(&:first)
-          rows = label_states.group_by { |h| h[:label_name] }.map { |k, v| { 'Label' => k }.merge(Hash[v.map { |a| [a[:destination], [a[:status], a[:errors]].compact.join(' ')] }]) }
 
+          header_cols, cols = publishing_table_headers(label_states)
+          rows = publishing_table_rows(label_states)
           layout = Crossbeams::Layout::Page.build({}) do |page|
-            if res.done
-              if res.failed
-                page.add_notice("Action failed #{res.errors}", notice_type: :error)
-              else
-                page.add_notice('Publish action is complete', notice_type: :success)
-              end
-            else
-              page.add_text '<div class="content-target content-loading"><div></div><div></div><div></div> Publishing in progress...'
-            end
+            current_state(page, res)
             page.add_table rows, cols.dup.unshift('Label'), header_captions: Hash[header_cols], cell_classes: publishing_table_classes(cols)
-
-            if res.done && published_labels?(label_states)
-              page.add_text(sql(label_states, res.chosen_printer), toggle_button: true, toggle_caption: 'Toggle SQL for template insert', syntax: :sql)
-            end
+            end_note(page, res, label_states)
           end
 
           layout
+        end
+
+        def self.publishing_table_headers(label_states)
+          header_cols = label_states.map { |h| [h[:destination], "#{h[:destination]}<br>#{h[:server_ip]}"] }.uniq
+          cols = header_cols.map(&:first)
+          [header_cols, cols]
+        end
+
+        def self.publishing_table_rows(label_states)
+          label_states.group_by { |h| h[:label_name] }.map { |k, v| { 'Label' => k }.merge(Hash[v.map { |a| [a[:destination], [a[:status], a[:errors]].compact.join(' ')] }]) }
+        end
+
+        def self.current_state(page, res)
+          if res.done
+            if res.failed
+              page.add_notice("Action failed #{res.errors}", notice_type: :error)
+            else
+              page.add_notice('Publish action is complete', notice_type: :success)
+            end
+          else
+            page.add_text '<div class="content-target content-loading"><div></div><div></div><div></div> Publishing in progress...'
+          end
+        end
+
+        def self.end_note(page, res, label_states)
+          return unless res.done && published_labels?(label_states)
+
+          if AppConst::LABEL_PUBLISH_NOTIFY_URLS.empty?
+            page.add_text(sql(label_states, res.chosen_printer), toggle_button: true, toggle_caption: 'Toggle SQL for template insert', syntax: :sql)
+          else
+            app_word = AppConst::LABEL_PUBLISH_NOTIFY_URLS.length == 1 ? 'application' : 'applications'
+            page.add_text("<strong>Note:</strong> Label layout notifications have been sent to #{AppConst::LABEL_PUBLISH_NOTIFY_URLS.length} #{app_word}.", wrapper: :p)
+          end
         end
 
         def self.publishing_table_classes(targets)
