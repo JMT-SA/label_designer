dd5013929b5b4e8a082c4ee2d5b399c38ef6ec4a
James Silberbauer <jamessil@telkomsa.net>
2021-02-22 09:24:22 +0200

    SRCC deploy change

--- config/deploy/srcc.rb
+++ config/deploy/srcc.rb
@@ -1,7 +1,7 @@
 # VM instance at SRCC.
-server '10.0.0.30', user: 'nsld', roles: %w[app db web]
+server '10.0.0.29', user: 'nsld', roles: %w[app db web]
 set :deploy_to, '/home/nsld/label_designer'
 set :ssh_options,
     forward_agent: true,
     keys: '~/.ssh/id_rsa'
-set :chruby_ruby, 'ruby-2.5.1'
+set :chruby_ruby, 'ruby-2.5.8'
